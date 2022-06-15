93d200ac339a2acbdd30813c735b0f97ea5fb16a
Venkata Kalyan Sagi <kalyansagi.555@gmail.com>
2022-06-14 23:29:08 -0400

    Taking only 2 features and creating the model. Changed the backward elimination also accordingly.

--- src/Multiple-Linear.py
+++ src/Multiple-Linear.py
@@ -7,16 +7,18 @@ import pandas as pd
 # Importing the dataset
 # ---------------------
 dataset = pd.read_csv('Multiple-Linear-Dataset.csv')
-X = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, 0:2].values
 y = dataset.iloc[:, 4].values
+print(X)
+print(y)
 
 # Encoding categorical data
 # -------------------------
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-import numpy as np
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
+# from sklearn.preprocessing import OneHotEncoder
+# from sklearn.compose import ColumnTransformer
+# import numpy as np
+# ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+# X = np.array(ct.fit_transform(X))
 
 # from sklearn.preprocessing import OneHotEncoder
 # from sklearn.compose import ColumnTransformer
@@ -25,7 +27,7 @@ X = np.array(ct.fit_transform(X))
 # X = ct.fit_transform(X)
 
 # Avoiding the Dummy Variable Trap
-X = X[:, 1:]
+# X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 # --------------------------------------------------------
@@ -57,18 +59,21 @@ print("RMSE is", score)
 # Building the optimal model using Backward Elimination
 import statsmodels.api as sm
 X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
-X_opt = np.array(X[:, [0, 1, 2, 3, 4, 5]], dtype=float)
+X_opt = np.array(X[:, [0, 1, 2]], dtype=float)
 regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+print("regressor_OLS.summary() with 2 features(Product_1 & Product_2)")
 print(regressor_OLS.summary())
-X_opt = np.array(X[:, [0, 1, 3, 4, 5]], dtype=float)
+X_opt = np.array(X[:, [0, 1]], dtype=float)
 regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+print("regressor_OLS.summary() with 1 features(Product_1)")
 print(regressor_OLS.summary())
-X_opt = np.array(X[:, [0, 3, 4, 5]], dtype=float)
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
-X_opt = np.array(X[:, [0, 3, 5]], dtype=float)
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
-X_opt = np.array(X[:, [0, 3]], dtype=float)
-regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
-print(regressor_OLS.summary())
+
+# X_opt = np.array(X[:, [0, 3, 4, 5]], dtype=float)
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# print(regressor_OLS.summary())
+# X_opt = np.array(X[:, [0, 3, 5]], dtype=float)
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# print(regressor_OLS.summary())
+# X_opt = np.array(X[:, [0, 3]], dtype=float)
+# regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# print(regressor_OLS.summary())
